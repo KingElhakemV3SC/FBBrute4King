15ee9676b9d61567de87f43d5855d1f96d756f6f
KingElhakemV3SC <91437786+KingElhakemV3SC@users.noreply.github.com>
2021-09-28 02:05:52 +0200

    Add files via upload

---
+++ KingElhakemV3SC.py
@@ -0,0 +1,57 @@
+#-*-coding:utf8;-*-
+#qpy:3
+#qpy:console
+
+import os
+
+A='\033[1;31m'
+B='\033[1;32m'
+C='\033[1;33m'
+D='\033[1;34m'
+E='\033[1;35m'
+F='\033[1;36m'
+grb='''@:[1864140940265166]'''
+print(B)
+os.system('cowsay -f eyes "KingElhakemV3$©™ " ')
+print(A)
+ok="""
+
+================================================================================
+========================================
+            KingElhakemV3$©™
+========================================================================================================================
+
+
+
+
+""".format(A, B, A,B, grb, A,C, A, C, A, C, A, C, A, C, A, C, A, C, A,'Welcome To @Termux@ ')
+print(ok)
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+import sys
+import mechanize
+import cookielib
+import random
+
+W = '\033[1;37;40m'
+Br = '\033[1;31;40m'
+Bg = '\033[1;32;40m'
+Y = '\033[1;33;40m'
+Bb = '\033[1;34;40m'
+Bm = '\033[1;35;40m'
+Bc = '\033[1;36;40m'
+import os
+
+A='\033[1;31m'
+B='\033[1;32m'
+C='\033[1;33m'
+D='\033[1;34m'
+E='\033[1;35m'
+F='\033[1;36m'
+grb='''@:[1864140940265166]'''
+print(B)
+os.system('cowsay -f eyes "dont forget to subscribe my channel on youtube " ')
+print(A)
+
+
